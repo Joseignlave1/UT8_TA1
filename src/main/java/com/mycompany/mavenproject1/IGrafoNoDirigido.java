acace291d78450a3fd8ef3e0f78a9e6ab2edb8ce
Joseignlave1 <jose.lavecchia@correo.ucu.edu.uy>
2024-06-05 20:59:07 -0300

    .

---
+++ src/main/java/com/mycompany/mavenproject1/IGrafoNoDirigido.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.mavenproject1;
+
+import java.util.Collection;
+import java.util.Map;
+
+public interface IGrafoNoDirigido {
+
+    public Collection <TVertice> bea();
+     public Collection <TVertice> bea(Comparable etiquetaOrigen);
+    public TGrafoNoDirigido Prim();
+
+    public TGrafoNoDirigido Kruskal();
+}
